4a265c1ec56e82ddfb1117592a6f8c9948fad129
Solomiles <osaighesolomon@gmail.com>
2024-01-02 09:45:06 +0100

    created diploma form migration,model and controller

---
+++ database/migrations/2024_01_02_073715_create_diplomas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diplomas', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('resume');
+            $table->string('program');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diplomas');
+    }
+};
--- resources/views/application-portal.blade.php
+++ resources/views/application-portal.blade.php
@@ -20,14 +20,13 @@
 </head>
 
 <body>
-	<!-- wrapper -->
-	<div class="wrapper">
+<!-- wrapper -->
+<div class="wrapper">
 		<div class="authentication-reset-password d-flex align-items-center justify-content-center">
 			<div class="row">
 				<div class="col-12 col-lg-10 mx-auto">
 					<div class="card rounded-5 overflow-hidden">
 						<div class="row g-0 align-items-center">
-							
 							<div class="col-lg-7 border-end">
 								<div class="card-body">
 									<div class="p-5">
@@ -37,118 +36,129 @@
 										<h4 class="mt-5 font-weight-bold">Application Portal</h4>
 										<p class="text-muted">Apply for any of our professional programme, you can apply for <br> a <strong>Diploma</strong> <span>or</span> as a <strong>Membership.</strong> </p> <br><br>
 										<ul class="nav nav-pills mb-3" role="tablist">
-										<li class="nav-item" role="presentation">
-											<a class="nav-link active" data-bs-toggle="pill" href="#primary-pills-home" role="tab" aria-selected="true">
-												<div class="d-flex align-items-center">
-													<div class="tab-icon"><i class="lni lni-graduation font-18 me-1"></i>
+											<li class="nav-item" role="presentation">
+												<a class="nav-link active" data-bs-toggle="pill" href="#primary-pills-home" role="tab" aria-selected="true">
+													<div class="d-flex align-items-center">
+														<div class="tab-icon"><i class="lni lni-graduation font-18 me-1"></i>
+														</div>
+														<div class="tab-title">Diploma</div>
 													</div>
-													<div class="tab-title">Diploma</div>
-												</div>
-											</a>
-										</li>
-										<li class="nav-item" role="presentation">
-											<a class="nav-link" data-bs-toggle="pill" href="#primary-pills-profile" role="tab" aria-selected="false" tabindex="-1">
-												<div class="d-flex align-items-center">
-													<div class="tab-icon"><i class="lni lni-user font-18 me-1"></i>
+												</a>
+											</li>
+											<li class="nav-item" role="presentation">
+												<a class="nav-link" data-bs-toggle="pill" href="#primary-pills-profile" role="tab" aria-selected="false" tabindex="-1">
+													<div class="d-flex align-items-center">
+														<div class="tab-icon"><i class="lni lni-user font-18 me-1"></i>
+														</div>
+														<div class="tab-title">Membership</div>
 													</div>
-													<div class="tab-title">Membership</div>
-												</div>
-											</a>
-										</li>
-									</ul>
+												</a>
+											</li>
+										</ul>
 									
 									<div class="tab-content" id="pills-tabContent">
+										
+											<div class="tab-pane fade active show" id="primary-pills-home" role="tabpanel">
 											<form id="diploma-form" method="POST">
-												@csrf
+											@csrf
+												<div class="mb-3 mt-5">
+													<label class="form-label">First Name</label>
+													<input type="text" name="firstname" class="form-control rounded-5" placeholder="First Name" />
+												</div>
+												<div class="mb-4">
+													<label class="form-label">Last Name</label>
+													<input type="text" name="lastname" class="form-control rounded-5" placeholder="Last Name" />
+												</div>
+												<div class="mb-4">
+													<label class="form-label">Email Address</label>
+													<input type="text" name="email" class="form-control rounded-5" placeholder="Email Address" />
+												</div>
+												<div class="mb-4">
+													<label for="formFile" class="form-label">Resume</label>
+													<input class="form-control rounded-5" name="resume" type="file" id="formFile">
+												</div>
+
+												<div class="mb-4">
+													<label for="formFile" class="form-label">What Programme are you applying for?</label>
+													<select name="program" class="form-select rounded-5" id="inputSelectCountry" aria-label="Default select example">
+															<option selected="">Select Programme</option>
+															<option>Diploma</option>
+															<option>Advance Diploma</option>
+															<option>Executive Diploma</option>
+														</select>
+												</div>
+
+												<div class="mb-4">
+													<label for="formFile" class="form-label">What country are you applying from?</label>
+													<select name="country" class="form-select rounded-5" id="inputSelectCountry" aria-label="Default select example">
+															<option selected="">Select Country</option>
+															<option>United State of America</option>
+															<option>Advance Diploma</option>
+															<option>Executive Diploma</option>
+														</select>
+												</div>
+												<div class="d-grid gap-3">
+													<button type="submit" class="btn btn-gradient-info rounded-5">Submit</button>
+													<a href="https://cilgglobal.org/" class="btn btn-white rounded-5"><i class='bx bx-arrow-back mr-1'></i>Back Home</a>
+												</div>
+											</form>
+											</div>
+										
+										
+											<div class="tab-pane fade" id="primary-pills-profile" role="tabpanel">
 												<div class="tab-pane fade active show" id="primary-pills-home" role="tabpanel">
-													<div class="mb-3 mt-5">
-														<label class="form-label">First Name</label>
-														<input type="text" name="firstname" class="form-control rounded-5" placeholder="First Name" />
-													</div>
-													<div class="mb-4">
-														<label class="form-label">Last Name</label>
-														<input type="text" name="lastname" class="form-control rounded-5" placeholder="Last Name" />
-													</div>
-													<div class="mb-4">
-														<label class="form-label">Email Address</label>
-														<input type="text" name="email" class="form-control rounded-5" placeholder="Email Address" />
-													</div>
-													<div class="mb-4">
-														<label for="formFile" class="form-label">Resume</label>
-														<input class="form-control rounded-5" name="resume" type="file" id="formFile">
-													</div>
-
-													<div class="mb-4">
-														<label for="formFile" class="form-label">What Programme are you applying for?</label>
-														<select name="program" class="form-select rounded-5" id="inputSelectCountry" aria-label="Default select example">
-																<option selected="">Select Programme</option>
-																<option>Diploma</option>
-																<option>Advance Diploma</option>
-																<option>Executive Diploma</option>
-															</select>
-													</div>
-
-													<div class="mb-4">
-														<label for="formFile" class="form-label">What country are you applying from?</label>
-														<select name="country" class="form-select rounded-5" id="inputSelectCountry" aria-label="Default select example">
-																<option selected="">Select Country</option>
-																<option>United State of America</option>
-																<option>Advance Diploma</option>
-																<option>Executive Diploma</option>
-															</select>
-													</div>
-													<div class="d-grid gap-3">
-														<button type="button" class="btn btn-gradient-info rounded-5">Submit</button>
-														<a href="https://cilgglobal.org/" class="btn btn-white rounded-5"><i class='bx bx-arrow-back mr-1'></i>Back Home</a>
-													</div>
-												</div>
-											</form>
-
 											<form id="membership-form" method="POST">
-												<div class="tab-pane fade" id="primary-pills-profile" role="tabpanel">
-													<div class="tab-pane fade active show" id="primary-pills-home" role="tabpanel">
-													<div class="mb-3 mt-5">
-														<label class="form-label">First Name</label>
-														<input type="text" name="firstname" class="form-control rounded-5" placeholder="First Name" />
-													</div>
-													<div class="mb-4">
-														<label class="form-label">Last Name</label>
-														<input type="text" name="lastname" class="form-control rounded-5" placeholder="Last Name" />
-													</div>
-													<div class="mb-4">
-														<label class="form-label">Email Address</label>
-														<input type="text" name="email" class="form-control rounded-5" placeholder="Email Address" />
-													</div>
-													<div class="mb-4">
-														<label for="formFile" class="form-label">Detailed Resume</label>
-														<input class="form-control rounded-5" name="resume" type="file" id="formFile">
-													</div>
-
-													<div class="mb-4">
-														<label for="formFile" class="form-label">Passport Photo</label>
-														<input class="form-control rounded-5" name="passport" type="file" id="formFile">
-													</div>
-
-													<div class="mb-4">
-														<label for="formFile" class="form-label">What country are you applying from?</label>
-														<select name="country" class="form-select rounded-5" id="inputSelectCountry" aria-label="Default select example">
-																<option selected="">Select Country</option>
-																<option>United State of America</option>
-																<option>Advance Diploma</option>
-																<option>Executive Diploma</option>
-															</select>
-													</div>
-													<div class="d-grid gap-3">
-														<button type="submit" onclick="submitApp()" class="btn btn-gradient-info submit-btn rounded-5">Submit</button>
-														<a href="https://cilgglobal.org/" class="btn btn-white rounded-5"><i class='bx bx-arrow-back mr-1'></i>Back Home</a>
-													</div>
+													@csrf
+												<div class="mb-3 mt-5">
+													<label class="form-label">First Name</label>
+													<input type="text" name="firstname" class="form-control rounded-5" placeholder="First Name" />
 												</div>
-											</form>
-										</div>
-									</div>	
+												<div class="mb-4">
+													<label class="form-label">Last Name</label>
+													<input type="text" name="lastname" class="form-control rounded-5" placeholder="Last Name" />
+												</div>
+												<div class="mb-4">
+													<label class="form-label">Email Address</label>
+													<input type="text" name="email" class="form-control rounded-5" placeholder="Email Address" />
+												</div>
+												<div class="mb-4">
+													<label for="formFile" class="form-label">Detailed Resume</label>
+													<input class="form-control rounded-5" name="resume" type="file" id="formFile">
+												</div>
+
+												<div class="mb-4">
+													<label for="formFile" class="form-label">Passport Photo</label>
+													<input class="form-control rounded-5" name="passport" type="file" id="formFile">
+												</div>
+
+												<div class="mb-4">
+													<label for="formFile" class="form-label">What country are you applying from?</label>
+													<select name="country" class="form-select rounded-5" id="inputSelectCountry" aria-label="Default select example">
+															<option selected="">Select Country</option>
+															<option>United State of America</option>
+															<option>Advance Diploma</option>
+															<option>Executive Diploma</option>
+														</select>
+												</div>
+												<div class="d-grid gap-3">
+													<button type="submit"  class="btn btn-gradient-info submit-btn rounded-5">
+														<span id="member-txt">Submit</span>
+														<div class="spinner-border spinner-border-sm btn-pr p-2" id="member-spinner"
+															style="display: none;" role="status">
+															<span class="sr-only">Loading...</span>
+														</div>
+													</button>
+													<a href="https://cilgglobal.org/" class="btn btn-white rounded-5"><i class='bx bx-arrow-back mr-1'></i>Back Home</a>
+												</div>
+												</form>
+											</div>
+										
 									</div>
-								</div>
+								</div>	
 							</div>
+						</div>
+					</div>
+							
 							
 							<div class="col-lg-5">
 								<div class="card-body">
@@ -177,12 +187,43 @@
 	<script src="assets/js/app.js"></script>
 
 	<script>
-		function submitApp(el) {
+		 $(document).ready(() => {
+			$('#diploma-form').submit(el => {
+			diplomaApp(el)
+			})
+
+			$('#membership-form').submit(el => {
+			memberApp(el)
+			})
+		});
+
+		function diplomaApp(el) {
 			el.preventDefault()
 
-			// spin('member')
+			// let url = `{{ url('members/add') }}`;
+    		let data = new FormData(el.target)
 			
-console.log('hit')
+console.log(data)
+			// let url = 'http://127.0.0.1:8000/';
+			// let data = new FormData(el.target)
+
+			// goPost(url, data)
+			// .then(res => {
+			// 	// spin('member')
+			// 	showAlert(true, res.message)
+			// })
+			// .catch(err => {
+			// 	handleFormError(err);
+			// })
+		}
+
+		function memberApp(el) {
+			el.preventDefault()
+
+			// let url = `{{ url('members/add') }}`;
+    		let data = new FormData(el.target)
+			
+console.log(data)
 			// let url = 'http://127.0.0.1:8000/';
 			// let data = new FormData(el.target)
 
